55acbe65d3bebe8576a5d07ee54ca91e88b79888
abdulhamit-dev <hmd62392@gmail.com>
2024-09-29 15:07:07 +0300

    added caching pipeline

---
+++ Nucleo.DDD.Application/Pipelines/Caching/CacheSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Nucleo.DDD.Application.Pipelines.Caching;
+
+public class CacheSettings
+{
+    public int SlidingExpiration {  set; get; }
+}
---
+++ Nucleo.DDD.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Nucleo.DDD.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    string CacheKey { get; }
+    bool BypassCache { get; }
+    string? CacheGroupKey { get; }
+    TimeSpan? SlidingExpiration { get; }
+}
