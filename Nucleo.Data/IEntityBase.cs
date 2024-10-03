7252f840a665ebbac29f2aa22bee826a34e33196
abdulhamit-dev <hmd62392@gmail.com>
2024-10-03 23:32:54 +0300

    Add Nucleo.Data project and update solution file

---
+++ Nucleo.Data/IEntityBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Nucleo.Data
+{
+    public interface IEntityBase
+    {
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
