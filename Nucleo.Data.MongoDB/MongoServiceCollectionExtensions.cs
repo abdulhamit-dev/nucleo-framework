e830a46cca5b86f8e300e90248ae3692d7cdd9e8
abdulhamit-dev <hmd62392@gmail.com>
2024-10-05 01:31:07 +0300

    Add DI extensions for MongoDB and update project references

---
+++ Nucleo.Data.MongoDB/MongoServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Nucleo.Data.MongoDB
+{
+    public static class MongoServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMongoDbRepositories(this IServiceCollection services, string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            services.AddSingleton<IMongoDatabase>(database);
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            return services;
+        }
+
+        public static IServiceCollection AddMongoCollection<T>(this IServiceCollection services, string collectionName) where T : class
+        {
+            services.AddScoped<IMongoCollection<T>>(sp =>
+            {
+                var database = sp.GetRequiredService<IMongoDatabase>();
+                return database.GetCollection<T>(collectionName);
+            });
+
+            return services;
+        }
+    }
+}
